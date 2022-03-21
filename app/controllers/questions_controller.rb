25da0cf6f0c2de167e9268fe3c7709f2055cca13
kelly@mavana.earth <kelly@mavana.earth>
2022-03-21 14:25:33 +0100

    EXO2 stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,21 @@
 class QuestionsController < ApplicationController
   def ask
+    # @questions = []
   end
 
   def answer
+    @questions = params[:question]
+    if @questions == "I am going to work"
+      @answers = 'Great ! '
+    elsif @questions.end_with?('?')
+      @answers = 'Silly question, get dressed and go to work!'
+    else
+      @answers = " I don't care, get dressed and go to work! "
+    end
+
   end
 end
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
